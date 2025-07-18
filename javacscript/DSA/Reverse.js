3187aeccfdc2fa5e6aad8bf28f9afdc8881e24df
shailendrayv <syadav620@gmail.com>
2025-07-18 23:06:43 +0530

    fact

--- javacscript/DSA/Reverse.js
+++ javacscript/DSA/Reverse.js
@@ -10,8 +10,12 @@ console.log(reverse(12334445));      // Output: "54443321"
 // factorial of a number
 let a=prompt("Enter fact ");
 function cal(a){
+      if(a<0){
+            throw new Error("not less than Zero");
+        }
     let fact=1;
     for(let i=1;i<=a;i++){
+      
         fact=fact*i;
     }
     return fact;
